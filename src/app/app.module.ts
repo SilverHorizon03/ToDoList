19ed7775b9dd80c187862882ed3aa8dcbbfa8403
Andrea <andrea.sensi04@gmail.com>
2022-09-29 17:29:04 +0200

    translations added to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
-import { ItemComponent } from './item/item.component';
+import { SaveComponent } from './save/save.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    ItemComponent
+    SaveComponent
   ],
   imports: [
     BrowserModule,
